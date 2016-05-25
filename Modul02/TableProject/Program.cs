a9dbda01d0382c7bb9233f5a2f83cec0365f1d35
pkamburov <pkamburov89@gmail.com>
2016-05-25 18:11:20 +0300

    Модул 02 - Работа с enum

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -4,23 +4,27 @@ namespace TableProject
 {
 	class MainClass
 	{
+		enum ti {Name=0, Surname, Years};
+
 		public static void Main (string[] args)
 		{
 			//Дефиниране на таблица
 			string[,] table = new string[2, 3];
 
 			//Въвеждане на първи ред
-			table[0, 0] = "John"; table[0, 1] = "Doe"; table[0, 2] = "29";
+			table[0, (int)ti.Name] = "John"; table[0, (int)ti.Surname] = "Doe"; table[0, (int)ti.Years] = "29";
 
 			//Въвеждане на втори ред
-			table[1, 0] = "Dohn"; table[1, 1] = "Joe"; table[1, 2] = "41";
+			table[1, (int)ti.Name] = "Dohn"; table[1, (int)ti.Surname] = "Joe"; table[1, (int)ti.Years] = "41";
 
 			//Печат
 			Console.Write ("Wich row you want to see: ");
 			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
 
-			Console.WriteLine ((table[_index, 0])[0] + ". " + table[_index, 1] + ", " + table[_index, 2] + " " +
-				"r.");
+			Console.WriteLine (
+				(table[_index, (int)ti.Name])[0] + ". " + 
+				table[_index, (int)ti.Surname] + ", " + 
+				table[_index, (int)ti.Years] + " " + "r.");
 		}
 	}
 }
