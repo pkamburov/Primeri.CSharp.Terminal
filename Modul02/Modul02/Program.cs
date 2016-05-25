474cb03796d5257f647bd63f981157bd811ae577
pkamburov <pkamburov89@gmail.com>
2016-05-25 15:41:35 +0300

    Модул 02 - Работа със стрингове

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -11,24 +11,27 @@ namespace Modul02
 
 			int a = 0, b = 0;
 			int sum = 0;
-
+			string program_name = "Modul02 - Examples";
+			string program_version = "1.0";
 			//Примери
 
 			a = 5; 
 			b = 10;
 			sum = a + b;
-
+			// За програмата
+			Console.WriteLine (program_name);
+			Console.WriteLine ("Version: " + program_version + "\n\n" );
 			// Писане в конзола
-			Console.Write ( a );
+			Console.Write (a);
 			Console.Write (" + ");
-			Console.Write ( b );
+			Console.Write (b);
 			Console.Write (" = ");
-			Console.WriteLine ( sum );
+			Console.WriteLine (sum);
 
 			//Допълнителни оператори
 			Console.WriteLine ("\nHow to use +=");
 			a += b; //a = 10 + 5, a = a + b;
-			Console.WriteLine ( a );
+			Console.WriteLine (a);
 
 			Console.WriteLine ("\nHow to use -=");
 			a -= b;
@@ -41,10 +44,9 @@ namespace Modul02
 			Console.WriteLine ("\nHow to use ++");
 			a++;
 			Console.WriteLine (a);
-			Console.ReadLine ();
 
-
-
+			Console.WriteLine ("Division: " + (7 / 3).ToString ()); // -> 2 (7 / 3 = 2,3333)
+			Console.WriteLine ("Division: " + (7 % 3).ToString ()); // -> 1 (1 / 3 = 0,3333)
 
 		}
 	}
