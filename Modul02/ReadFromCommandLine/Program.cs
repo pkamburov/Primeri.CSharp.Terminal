cbb2e443681980d2c2ba746d3349c58b1114e177
pkamburov <pkamburov89@gmail.com>
2016-05-25 16:28:58 +0300

    Модул 02 - Работа със стрингове

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -15,6 +15,19 @@ namespace ReadFromCommandLine
 
 			//Печат на резултат
 			Console.WriteLine ( "Result is " + (a + b).ToString() + "\n\n\n");
+
+			string test = "1";
+			test += ", 2";
+			test += ", 100";
+			test += ", 4";
+			test += ", 5";
+
+			Console.WriteLine (" += : " + test + "\n\n");
+
+			test = test.Replace (", ", ";");
+			Console.WriteLine ("Replace : " + test + "\n");
+
+			Console.WriteLine ("3rd sign: " + test.Split (';') [2]);
 		}
 	}
 }
